974bfd3bb508e33116ea75fa84bca66a7b9f154e
Julien <julien.lopinto@gmail.com>
2022-01-22 15:03:43 +0100

    PoC

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Hello from 'svelte-materials/Hello.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>svelte-materials</h1>
+	<Hello />
+</section>
