c6c9e4a03d720eea80abfa50c508fa0b8f2990ea
Julien <julien.lopinto@gmail.com>
2022-01-25 21:15:34 +0100

    improving collapse demo

--- src/routes/components/collapse.svelte
+++ src/routes/components/collapse.svelte
@@ -7,16 +7,16 @@
 	let collapse2IsOpen = true;
 </script>
 
-<Button on:click={() => collapse1.open()} label="Open" />
-<Button on:click={() => collapse1.close()} label="Close" />
+<Button disabled={collapse1IsOpen} on:click={() => collapse1.open()} label="Open" />
+<Button disabled={!collapse1IsOpen}  on:click={() => collapse1.close()} label="Close" />
 <Button on:click={() => (collapse1IsOpen ? collapse1.close() : collapse1.open())} label="Toggle" />
 
 <Collapse bind:controls={collapse1} bind:isOpen={collapse1IsOpen}>
-	<p>
+	<div>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget ex euismod, euismod nunc eu,
 		consectetur nunc. Sed euismod, urna eu tincidunt tincidunt, nisi nisl tincidunt nunc, euismod
 		aliquet nunc nisl euismod nunc.
-	</p>
+	</div>
 	<div>
 		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque iste consequuntur sint tempore
 		praesentium. Porro obcaecati debitis, quo, nostrum soluta atque quis dolores est quisquam
@@ -44,7 +44,7 @@
 <Button disabled={!collapse2IsOpen} on:click={() => collapse2.close()} label="Close" />
 <Button
 	on:click={() => (collapse2IsOpen ? collapse2.close() : collapse2.open())}
-	label="Toggle, is open ? {collapse2IsOpen}"
+	label="Toggle"
 />
 
 <Collapse bind:controls={collapse2} bind:isOpen={collapse2IsOpen}>
